3f10759a80211a14564ce8a89f94c389ed5ed1fc
Migal <migal@tozny.com>
2025-06-04 13:50:05 -0400

    Cleaning up reset password code

--- js/apps/admin-ui/src/user/user-credentials/ResetPasswordDialog.tsx
+++ js/apps/admin-ui/src/user/user-credentials/ResetPasswordDialog.tsx
@@ -1,6 +1,7 @@
 import { RequiredActionAlias } from "@keycloak/keycloak-admin-client/lib/defs/requiredActionProviderRepresentation";
 import type UserRepresentation from "@keycloak/keycloak-admin-client/lib/defs/userRepresentation";
 import {
+  ActionGroup,
   AlertVariant,
   Button,
   ButtonVariant,
@@ -12,12 +13,9 @@ import {
 } from "@patternfly/react-core";
 import { FormProvider, useForm } from "react-hook-form";
 import { useTranslation } from "react-i18next";
-import { FormErrorText, HelpItem, PasswordInput } from "@keycloak/keycloak-ui-shared";
 import { useAdminClient } from "../../admin-client";
-import { DefaultSwitchControl } from "../../components/SwitchControl";
 import { useAlerts } from "@keycloak/keycloak-ui-shared";
 import {
-  ConfirmDialogModal,
   useConfirmDialog,
 } from "../../components/confirm-dialog/ConfirmDialog";
 import useToggle from "../../utils/useToggle";
@@ -58,9 +56,7 @@ export const ResetPasswordDialog = ({
     mode: "onChange",
   });
   const {
-    register,
     formState: { isValid, errors },
-    watch,
     handleSubmit,
     clearErrors,
     setError,
@@ -91,7 +87,6 @@ export const ResetPasswordDialog = ({
           : t("savePasswordSuccess"),
         AlertVariant.success,
       );
-      //refresh();
     } catch (error) {
       addError(
         isResetPassword ? "resetPasswordError" : "savePasswordError",
@@ -99,138 +94,53 @@ export const ResetPasswordDialog = ({
       );
     }
 
-    //onClose();
   };
 
   return (
     <>
-      <FormProvider {...form}>
+      <FormProvider {...form} >
         <Modal
           title={t("resetPasswordFor", { username: user.username })}
           isOpen={confirm}
           onClose={onClose}
           variant={ModalVariant.small}
-          actions={[
-            <Button
-              id="modal-confirm"
-              data-testid="confirm"
-              key="confirm"
-              isDisabled={!isValid}
-              variant={ButtonVariant.primary}
-              onClick={form.handleSubmit((data) => { saveUserPassword(data) })}
-            >
-              {t("save")}
-            </Button>,
-            <Button
-              id="modal-cancel"
-              data-testid="cancel"
-              key="cancel"
-              variant={ButtonVariant.link}
-              onClick={() => {
-                if (onClose) onClose();
-                toggle();
-              }}
-            >
-              {t("cancel")}
-            </Button>
-
-          ]}
         >
-          <Form>
+          <Form onSubmit={form.handleSubmit(saveUserPassword)}>
             <ToznyPasswordBrokerFields realm={realm!} />
             <FormGroup
-              label={t("testConnectionRedirectURI")}
-              labelIcon={
-                <HelpItem helpText={t("testConnectionRedirectURIHelp")} fieldLabelId="testConnectionRedirectURI" />
-              }
+              label={t("resetPasswordLink")}
               fieldId="kc-reset-password-uri"
             >
               <ClipboardCopy
                 isReadOnly
               >{resetLink}</ClipboardCopy>
             </FormGroup>
+            <ActionGroup>
+              <Button
+                id="reset-submit"
+                data-testid="submit"
+                key="submit"
+                isDisabled={!isValid}
+                variant={ButtonVariant.primary}
+              >
+                {t("resetPassword")}
+              </Button>,
+              <Button
+                id="modal-cancel"
+                data-testid="cancel"
+                key="cancel"
+                variant={ButtonVariant.link}
+                onClick={() => {
+                  if (onClose) onClose();
+                  toggle();
+                }}
+              >
+                {t("cancel")}
+              </Button>
+            </ActionGroup>
           </Form>
         </Modal>
       </FormProvider>
-      {/* <ConfirmSaveModal />
-      <ConfirmDialogModal
-        titleKey={
-          isResetPassword
-            ? t("resetPasswordFor", { username: user.username })
-            : t("setPasswordFor", { username: user.username })
-        }
-        open={confirm}
-        onCancel={onClose}
-        toggleDialog={toggle}
-        onConfirm={toggleConfirmSaveModal}
-        confirmButtonDisabled={!isValid}
-        continueButtonLabel="save"
-      >
-        <Form
-          id="userCredentials-form"
-          isHorizontal
-          className="keycloak__user-credentials__reset-form"
-        >
-          <FormGroup
-            name="password"
-            label={t("password")}
-            fieldId="password"
-            isRequired
-          >
-            <PasswordInput
-              data-testid="passwordField"
-              id="password"
-              onChange={(e) => {
-                onChange(e);
-                if (passwordConfirmation !== e.currentTarget.value) {
-                  setError("passwordConfirmation", {
-                    message: t("confirmPasswordDoesNotMatch").toString(),
-                  });
-                } else {
-                  clearErrors("passwordConfirmation");
-                }
-              }}
-              {...rest}
-            />
-            {errors.password && <FormErrorText message={t("required")} />}
-          </FormGroup>
-          <FormGroup
-            name="passwordConfirmation"
-            label={
-              isResetPassword
-                ? t("resetPasswordConfirmation")
-                : t("passwordConfirmation")
-            }
-            fieldId="passwordConfirmation"
-            isRequired
-          >
-            <PasswordInput
-              data-testid="passwordConfirmationField"
-              id="passwordConfirmation"
-              {...register("passwordConfirmation", {
-                required: true,
-                validate: (value) =>
-                  value === password ||
-                  t("confirmPasswordDoesNotMatch").toString(),
-              })}
-            />
-            {errors.passwordConfirmation && (
-              <FormErrorText
-                message={errors.passwordConfirmation.message as string}
-              />
-            )}
-          </FormGroup>
-          <FormProvider {...form}>
-            <DefaultSwitchControl
-              name="temporaryPassword"
-              label={t("temporaryPassword")}
-              labelIcon={t("temporaryPasswordHelpText")}
-              className="pf-v5-u-mb-md"
-              defaultValue="true"
-            />
-          </FormProvider>
-        </Form>
-      </ConfirmDialogModal> */}
     </>
   );
 };
