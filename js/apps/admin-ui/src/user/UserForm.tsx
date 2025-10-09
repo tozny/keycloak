c01043e062715f672a9b1af9b5fa5289fe53beef
Karthik P <karthik@datacaliper.com>
2025-10-09 17:41:28 +0530

    Make fn, ln and email mandatory.

--- js/apps/admin-ui/src/user/UserForm.tsx
+++ js/apps/admin-ui/src/user/UserForm.tsx
@@ -328,6 +328,7 @@ export const UserForm = ({
               label={t("email")}
               type="email"
               rules={{
+                required: t("required"),
                 pattern: {
                   value: emailRegexPattern,
                   message: t("emailInvalid"),
@@ -343,8 +344,8 @@ export const UserForm = ({
             />
 
             <ToznyPasswordBrokerFields realm={realm} />
-            <TextControl name="firstName" label={t("firstName")} />
-            <TextControl name="lastName" label={t("lastName")} />
+            <TextControl name="firstName" label={t("firstName")} rules={{ required: t("required") }} />
+            <TextControl name="lastName" label={t("lastName")} rules={{ required: t("required") }}/>
           </>
         )}
         {isBruteForceProtected && (
