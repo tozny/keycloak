2f593ac4fb67c0f772170dc15669f4fc29b73368
praveendc <praveenkumar@datacaliper.com>
2025-11-17 18:33:06 +0530

    Changed auth session from context and added directly to get auth note

--- services/src/main/java/org/keycloak/authentication/AuthenticationProcessor.java
+++ services/src/main/java/org/keycloak/authentication/AuthenticationProcessor.java
@@ -614,8 +614,8 @@ public class AuthenticationProcessor {
                     .queryParam(Constants.TAB_ID, getAuthenticationSession().getTabId())
                     .queryParam(Constants.CLIENT_DATA, getClientData());
             // Toz custom code to use encoded auth session ID
-            AuthenticationSessionModel authSession = session.getContext().getAuthenticationSession();
-            String authSessionNote = authSession.getAuthNote("ENCODED_AUTH_SESSION_ID");
+//            AuthenticationSessionModel authSession = session.getContext().getAuthenticationSession();
+            String authSessionNote = session.getContext().getAuthenticationSession().getAuthNote("ENCODED_AUTH_SESSION_ID");
             if (getUriInfo().getQueryParameters().containsKey(LoginActionsService.AUTH_SESSION_ID)) {
                 uriBuilder.queryParam(LoginActionsService.AUTH_SESSION_ID, authSessionNote);
             }
